255d9ab4a77073f632c2bb40e52b21fe05d265bc
AngellyVG <angelly.velizg@ug.edu.ec>
2024-07-08 21:28:09 -0500

    Add files via upload

--- weather_app.v2/main.py
+++ weather_app.v2/main.py
@@ -1,7 +1,27 @@
+
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template,session,redirect,url_for,request
+
+#DATABASE = 'C:\Users\gig2024\Desktop\weather_app.v2\db.sqlite'
 
 app = Flask(__name__)
 app.secret_key= 'ANGEVEGA'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+
+
+db = SQLAlchemy (app)  
+
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.Text, nullable=False)
+    password = db.Column(db.Text, nullable=False)
+    nombre = db.Column(db.Text, nullable=False)
+
+with app.app_context():
+    db.create_all()
+
+
 @app.route("/")
 def index():
     return render_template('index.html')
@@ -19,14 +39,26 @@ def segura():
 @app.route("/login", methods= ['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        return redirect(url_for('segura.html'))
+        email = str(request.form.get ( 'email'))
+        password = request.form.get ('password')
+        usuario = Usuario.query.filter_by(email=email).first()
+        if usuario: 
+            session['name']=usuario.nombre
+            return redirect(url_for('segura'))     
     return render_template('login.html')
+
+@app.route("/logout")
+def logout ():
+    session.clear()
+    return redirect_templates((url_for)('login'))
+    
+    
 
 if __name__ == "__main__":
         app.run(debug=True)
 
 
-
+@app.route("/logout")
 
 
 
